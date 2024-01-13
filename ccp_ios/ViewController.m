ae3aabbb5438192f52c2d6d1feff32c943c44561
xsluo <xsluo@126.com>
2024-01-13 15:56:05 +0800

    除设置以外正确

--- ccp_ios/ViewController.m
+++ ccp_ios/ViewController.m
@@ -17,22 +17,14 @@
 @interface ViewController ()<UITableViewDelegate,UITableViewDataSource,UITextFieldDelegate,AVCaptureMetadataOutputObjectsDelegate>
 @property (retain, nonatomic)  MBProgressHUD *hud;
 @property (nonatomic,retain) NSMutableArray <CBPeripheral*> *devices;;
-@property (nonatomic,retain) NSMutableArray *localNames;
 @property (nonatomic,strong) NSString *brand;
 @property (nonatomic,strong) UIView *viewMusk;
 @property (nonatomic,strong) UITableView *tableview;
 @property (nonatomic,strong)  UIButton *btclose;
-//@property (nonatomic,retain)  UIView * viewPass;  //密码界面
-//@property(nonatomic, strong) UITextField *tfPass1;
-//@property(nonatomic, strong) UITextField *tfPass2;
-//@property(nonatomic, strong) UITextField *tfPass3;
 @property(nonatomic,strong) NSString *strpass;
 @property Byte bytePass1;
 @property Byte bytePass2;
 @property Byte bytePass3;
-
-@property NSString *deviceprofix;
-
 @property (nonatomic,retain)  AVCaptureSession *session; //扫描二维码会话
 @property (nonatomic,retain)  AVCaptureVideoPreviewLayer *layer;
 
@@ -50,7 +42,6 @@
     self.tableview.delegate = self;
     self.tableview.dataSource = self;
     self.devices = [[NSMutableArray alloc]init];
-    self.localNames = [[NSMutableArray alloc]init];
     [self setAutoLayout];
     [self creatview];
     baby = [BabyBluetooth shareBabyBluetooth];
@@ -58,11 +49,9 @@
 }
 
 -(void)viewDidAppear:(BOOL)animated{
-   // [self.viewMusk setHidden:YES];
     [self.tableview reloadData];
     [self babyDelegate];
     baby.scanForPeripherals().begin();
-    //[self.viewMusk setHidden:NO];
 }
 
 -(void)setAutoLayout{
@@ -76,20 +65,20 @@
     [self.view addSubview:viewTop];
     [viewTop setBackgroundColor:[UIColor colorWithRed:105.0/255 green:79.0/255 blue:83.0/255 alpha:1.0]];
     viewTop.sd_layout
-    .leftEqualToView(self.view)
-    .rightEqualToView(self.view)
-    .topEqualToView(self.view)
-    .heightRatioToView(self.view, 0.1);
+        .leftEqualToView(self.view)
+        .rightEqualToView(self.view)
+        .topEqualToView(self.view)
+        .heightRatioToView(self.view, 0.1);
     
     //商标
     UIImageView *imglogo = [UIImageView new];
     [self.view addSubview:imglogo];
     [imglogo setImage:[UIImage imageNamed:@"MYCOOLMAN"]];
     imglogo.sd_layout
-    .centerXEqualToView(self.view)
-    .centerYIs(viewY * 0.07)
-    .widthIs(viewX*0.443)
-    .heightRatioToView(self.view, 0.031);
+        .centerXEqualToView(self.view)
+        .centerYIs(viewY * 0.07)
+        .widthIs(viewX*0.443)
+        .heightRatioToView(self.view, 0.031);
     
     //返回
     //47 86 ，77，136
@@ -97,10 +86,10 @@
     [self.view addSubview:btReturn];
     [btReturn setImage:[UIImage imageNamed:@"APP-Surface16_05"] forState:UIControlStateNormal];
     btReturn.sd_layout
-    .leftSpaceToView(self.view, 0.062*viewX)
-    .topSpaceToView(self.view, 0.054*viewY)
-    .widthIs(0.039*viewX)
-    .heightIs(0.031*viewY);
+        .leftSpaceToView(self.view, 0.062*viewX)
+        .topSpaceToView(self.view, 0.054*viewY)
+        .widthIs(0.039*viewX)
+        .heightIs(0.031*viewY);
     
     //设置
     //673 88 ，721，698
@@ -108,10 +97,10 @@
     [self.view addSubview:btSetting];
     [btSetting setImage:[UIImage imageNamed:@"setting"] forState:UIControlStateNormal];
     btSetting.sd_layout
-    .leftSpaceToView(self.view, 0.876*viewX)
-    .topSpaceToView(self.view, 0.055*viewY)
-    .widthIs(0.062*viewX)
-    .heightEqualToWidth();
+        .leftSpaceToView(self.view, 0.876*viewX)
+        .topSpaceToView(self.view, 0.055*viewY)
+        .widthIs(0.062*viewX)
+        .heightEqualToWidth();
     
     
     //文字 recreational seris
@@ -119,10 +108,10 @@
     [imgRCeational setImage:[UIImage imageNamed:@"APP-Surface10_05"]];
     [self.view addSubview:imgRCeational];
     imgRCeational.sd_layout
-    .centerXEqualToView(self.view)
-    .widthRatioToView(self.view, 0.88)
-    .centerYIs(viewY*0.222)
-    .heightIs(viewY*0.115);
+        .centerXEqualToView(self.view)
+        .widthRatioToView(self.view, 0.88)
+        .centerYIs(viewY*0.222)
+        .heightIs(viewY*0.115);
     
     //中间提示文字
     UILabel *lbInstruction = [UILabel new];
@@ -133,10 +122,10 @@
     [lbInstruction sizeToFit];
     [lbInstruction setText:@"To connect your MyCoolman\nwith Bluetooth\nensure the cooler is set to Bluetooth ON"];
     lbInstruction.sd_layout
-    .centerXEqualToView(self.view)
-    .centerYIs(viewY*0.42)
-    .widthIs(viewX*0.622)
-    .heightIs(viewY*0.142);
+        .centerXEqualToView(self.view)
+        .centerYIs(viewY*0.42)
+        .widthIs(viewX*0.622)
+        .heightIs(viewY*0.142);
     [lbInstruction setAdjustsFontSizeToFitWidth:YES];
     
     //扫描二维码
@@ -145,13 +134,13 @@
     [btScan setBackgroundColor:[UIColor colorWithRed:105.0/255 green:79.0/255 blue:83.0/255 alpha:1.0]];
     [btScan setTitle:@"Scan QR Code" forState:UIControlStateNormal];
     btScan.sd_layout
-    .centerXEqualToView(self.view)
-    .centerYIs(viewY*0.6)
-    .widthIs(viewX*0.44)
-    .heightIs(viewY*0.08);
+        .centerXEqualToView(self.view)
+        .centerYIs(viewY*0.6)
+        .widthIs(viewX*0.44)
+        .heightIs(viewY*0.08);
     [btScan setSd_cornerRadius:@8.0f];
     [btScan addTarget:self action:@selector(scanQRcode) forControlEvents:UIControlEventTouchUpInside];
-
+    
     //下面提示文字
     UILabel *lbInstr = [UILabel new];
     [self.view addSubview:lbInstr];
@@ -163,10 +152,10 @@
     [lbInstr setAdjustsFontSizeToFitWidth:YES];
     
     lbInstr.sd_layout
-    .centerXEqualToView(self.view)
-    .centerYIs(viewY*0.7)
-    .widthIs(viewX*0.618)
-    .heightIs(viewY*0.142);
+        .centerXEqualToView(self.view)
+        .centerYIs(viewY*0.7)
+        .widthIs(viewX*0.618)
+        .heightIs(viewY*0.142);
     
     //扫描蓝牙
     UIButton *btBluetooth = [UIButton new];
@@ -174,10 +163,10 @@
     //[btBluetooth setBackgroundColor:[UIColor colorWithRed:200.0/255 green:101.0/255 blue:69.0/255 alpha:1.0]];
     [btBluetooth setBackgroundImage:[UIImage imageNamed:@"APP-Surface3_47"] forState:UIControlStateNormal];
     btBluetooth.sd_layout
-    .centerXEqualToView(self.view)
-    .centerYIs(viewY*0.84)
-    .heightIs(viewY*0.08)
-    .widthEqualToHeight();
+        .centerXEqualToView(self.view)
+        .centerYIs(viewY*0.84)
+        .heightIs(viewY*0.08)
+        .widthEqualToHeight();
     [btBluetooth setSd_cornerRadius:@8.0f];
     [btBluetooth addTarget:self action:@selector(bleTouched) forControlEvents:UIControlEventTouchUpInside];
 }
@@ -186,9 +175,6 @@
 #pragma  - mark 弹窗
 -(void)creatview{
     CGRect parentFrame = self.view.frame;
-    float rwidth = parentFrame.size.width/390;
-    float rheight = parentFrame.size.height/844;
-    
     CGRect muskFrame = CGRectMake(parentFrame.origin.x, parentFrame.origin.y, parentFrame.size.width, parentFrame.size.height);
     self.viewMusk = [[UIView alloc] initWithFrame:muskFrame];
     [self.viewMusk setBackgroundColor:[UIColor colorWithRed:1/255.0 green:1/255.0 blue:1/255.0 alpha:0.5]];
@@ -225,32 +211,11 @@
     [baby setBlockOnDiscoverToPeripherals:^(CBCentralManager *central, CBPeripheral *peripheral, NSDictionary *advertisementData, NSNumber *RSSI) {
         NSLog(@"Device discovered :%@",peripheral.name);
         
-        /*
-        NSString *advertiseName = advertisementData[@"Local Name"];
-        if([advertiseName hasPrefix:@"CCP15R"]||[advertiseName hasPrefix:@"CCP15R"]||[advertiseName hasPrefix:@"EVA"]||[advertiseName hasPrefix:@"GCA"])  {
-            [weakSelf.devices addObject:peripheral];
-            [weakSelf.localNames addObject:advertiseName];
-            // weakSelf.currPeripheral = peripheral;
-            [weakSelf.tableview reloadData];
-            if([weakSelf.devices count]>5){
-                [central stopScan];
-            }
-            if([advertiseName hasPrefix:@"EVA24"]) {
-                weakSelf.brand = @"EVA24VTR";
-            }else if([advertiseName hasPrefix:@"EVA12"]){
-                weakSelf.brand = @"EVA12VTR";
-            }else{
-                weakSelf.brand = @"EVA2700RV";
-            }
-        }
-        */
-    
         if([peripheral.name hasPrefix:@"CCP15R"] ||[peripheral.name hasPrefix:@"CCP20R"]){
             [weakSelf.devices addObject:peripheral];
             [weakSelf.tableview reloadData];
-          //  [baby.centralManager connectPeripheral:peripheral options:nil];
+            //  [baby.centralManager connectPeripheral:peripheral options:nil];
         }
-        // [weakSelf.tableView reloadData];
         if([weakSelf.devices count]>5){
             [central stopScan];
         }
@@ -349,30 +314,6 @@
             if(data.length == 0){
                 [weakSelf getPassWord];
             }
-            /*
-             @property Byte  start; //通讯开始
-             @property Byte  power; //0x01开机，0x00关机
-             @property Byte  tempcool;  //设定制冷温度
-             @property Byte  tempReal;  //实时制冷温度或加热温度
-             @property Byte  frozesetting;  //   冷冻箱设定温度 （双温区冰箱才有效，单温区冰箱忽略该数据）
-             @property Byte  frozereal;  // 冷冻箱实时温度 （双温区冰箱才有效，单温区冰箱忽略该数据）
-             @property Byte  turbo;  //TURBOM模式 0X00-ECO  0X01-TURBO
-             @property Byte  heat; //加热或制冷模式 0X00-制冷模式 0X01-加热模式（单冷冰箱没有此值）
-             @property Byte  battery;  //电池保护设置 0-低档 1-中档 2-高档
-             @property Byte  unit;  //温度单温 0-华氏度 1-摄氏度
-             @property Byte  status;  //工作状态  0-停机 1-工作
-             @property Byte  errorcode;  //故障代码
-             @property Byte  vhigh;   //电压高八位
-             @property Byte  vlow;  //电压低八位
-             @property Byte  gc;    //GC单温冰箱 固定0x03
-             @property Byte  tempheat;   //加热设定温度
-             @property Byte  timer;    //定时关机时间
-             @property Byte  code1;    //备用
-             @property Byte  code2;    //备用
-             @property Byte  crcH;  //CRC 校验高八位
-             @property Byte  crcL;  //CRC 校验高八位
-             @property Byte  end;  //通讯结束
-             */
             
             if(data.length == 22){
                 memcpy(r, [data bytes], 22);
@@ -401,7 +342,6 @@
                 weakSelf.dataRead.end = r[21];  //通信结束
                 if(self.dataRead.gc == 0x00){
                     NSLog(@"跳到密码输入界面");
-                   // [weakSelf.viewPass setHidden:NO];
                     [weakSelf.viewMusk setHidden:YES];
                     PassViewController  *passViewController = [PassViewController new];
                     [passViewController setModalPresentationStyle:UIModalPresentationFullScreen];
@@ -422,46 +362,22 @@
     
     //设置连接的设备的过滤器
     
-     __block BOOL isFirst = YES;
-     [baby setFilterOnConnectToPeripherals:^BOOL(NSString *peripheralName, NSDictionary *advertisementData, NSNumber *RSSI) {
-     if(isFirst && ([peripheralName hasPrefix:@"CCP15R"]|| [peripheralName hasPrefix:@"CCP20R"])){
-     isFirst = NO;
-     return YES;
-     }
-     return NO;
-     }];
+    __block BOOL isFirst = YES;
+    [baby setFilterOnConnectToPeripherals:^BOOL(NSString *peripheralName, NSDictionary *advertisementData, NSNumber *RSSI) {
+        if(isFirst && ([peripheralName hasPrefix:@"CCP15R"]|| [peripheralName hasPrefix:@"CCP20R"])){
+            isFirst = NO;
+            return YES;
+        }
+        return NO;
+    }];
 }
 
 //按下蓝牙按钮
-
 -(void)bleTouched{
     [self.viewMusk setHidden:NO];
     [self.tableview setHidden:NO];
     [self.btclose setHidden:NO];
 }
-
--(void) deviceSelected{
-    //baby.scanForPeripherals().begin();
-    //[self.viewMusk setHidden:NO];
-   // [self.tableview setHidden:NO];
-    //baby.scanForPeripherals().begin();
-    
-//    [self.tfPass1 setHidden:YES];
-//    [self.tfPass2 setHidden:YES];
-//    [self.tfPass3 setHidden:YES];
-    
-   // self.hud = [[MBProgressHUD alloc]initWithView:self.body];
-    self.hud = [[MBProgressHUD alloc]initWithView:self.viewMusk];
-    [self.viewMusk addSubview:self.hud];
-    self.hud.mode = MBProgressHUDModeIndeterminate;
-    
-    [self.hud setOffset:CGPointMake(0, -150 *self.view.height/844.0)];
-    //self.hud.label.text = @"Scan bluetooth";
-    self.hud.label.text = NSLocalizedString(@"connect", nil);
-    [self.hud showAnimated:YES];
-    [self.hud hideAnimated:YES afterDelay:10];
-}
-
 
 #pragma mark tableviewdatasource
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
@@ -478,7 +394,7 @@
     [cell setBackgroundColor:[UIColor whiteColor]];
     
     CBPeripheral *peripheral = [self.devices objectAtIndex:indexPath.row];
-   // NSString *advertiseName = [self.localNames objectAtIndex:indexPath.row];
+    // NSString *advertiseName = [self.localNames objectAtIndex:indexPath.row];
     [cell.textLabel setText:peripheral.name];
     [cell.textLabel setTextColor:[UIColor blackColor]];
     
@@ -493,7 +409,7 @@
     [baby.centralManager stopScan];
     [baby cancelAllPeripheralsConnection];
     [baby.centralManager connectPeripheral:[self.devices objectAtIndex:indexPath.row] options:nil];
-
+    
     
     self.hud = [MBProgressHUD showHUDAddedTo:self.viewMusk animated:YES];
     self.hud.mode = MBProgressHUDModeIndeterminate;
@@ -514,9 +430,6 @@
 //获取密码
 -(void)getPassWord{
     //大板 A46
-//    [self.tfPass1 setHidden:NO];
-//    [self.tfPass2 setHidden:NO];
-//    [self.tfPass3 setHidden:NO];
     
     if(self.characteristic != nil){
         Byte  write[8];
@@ -600,57 +513,20 @@
                 [self.session stopRunning];
                 //3.移除预览图层
                 [self.layer removeFromSuperlayer];
-            }
-        }
-        
-        
-        /*
-        int i=0;
-        for(i=0;i<self.devices.count;i++){
-            if([[self.devices objectAtIndex:i].name isEqualToString:strtype]){
-                [self.viewMusk setHidden:NO];
-                [self.tableview setHidden:YES];
-                [self.btclose setHidden:YES];
-                //[self.viewPass setHidden:YES];
-                [baby.centralManager stopScan];
-                [baby cancelAllPeripheralsConnection];
-                [baby.centralManager connectPeripheral:[self.devices objectAtIndex:i] options:nil];
-                //2.停止会话
-                [self.session stopRunning];
-                //3.移除预览图层
-                [self.layer removeFromSuperlayer];
                 return;
             }
-        }*/
-        //没有找到设备
-       
-            self.hud.mode = MBProgressHUDModeText;
-            [self.view addSubview:self.hud];
-            self.hud.label.text = @"Device not found!";
-            [self.hud setMinShowTime:3];
-            [self.hud showAnimated:YES];
-            [self.hud hideAnimated:YES];
+        }
         
-    
-        /*
-        int i=0;
-        for(i=0;i<self.devices.count;i++){
-            if([[self.devices objectAtIndex:i].name hasPrefix:strtype]){
-                [baby.centralManager stopScan];
-                [baby cancelAllPeripheralsConnection];
-                [baby.centralManager connectPeripheral:[self.devices objectAtIndex:i] options:nil];
-            }
-        }
+        
         //没有找到设备
-        if(i==self.devices.count){
-            self.hud.mode = MBProgressHUDModeText;
-            [self.view addSubview:self.hud];
-            self.hud.label.text = @"Device not found!";
-            [self.hud setMinShowTime:3];
-            [self.hud showAnimated:YES];
-            [self.hud hideAnimated:YES];
-        }
-        */
+        
+        self.hud.mode = MBProgressHUDModeText;
+        [self.view addSubview:self.hud];
+        self.hud.label.text = @"Device not found!";
+        [self.hud setMinShowTime:3];
+        [self.hud showAnimated:YES];
+        [self.hud hideAnimated:YES];
+        
         //2.停止会话
         [self.session stopRunning];
         //3.移除预览图层
@@ -664,17 +540,6 @@
     //有返回非零数字，密码正确。保存密码
     if( self.dataRead.gc!= 0 ){
         [self.viewMusk setHidden:YES];
-//        if(self.strpass == nil){
-//            //保存密码
-//            NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-//            self.strpass = [NSString stringWithFormat:@"%@%@%@",self.tfPass1.text,self.tfPass2.text,self.tfPass3.text];
-//            [defaults setValue:self.strpass forKey:self.currPeripheral.identifier.UUIDString];
-//            self.bytePass1 = (Byte)strtoul([self.tfPass1.text UTF8String],0,16);  //16进制字符串转换成int
-//            self.bytePass2 = (Byte)strtoul([self.tfPass2.text UTF8String],0,16);  //16进制字符串转换成int
-//            self.bytePass3 = (Byte)strtoul([self.tfPass3.text UTF8String],0,16);  //16进
-//        }
-    
-        [self.viewMusk setHidden:YES];
         //  [baby.centralManager  stopScan];
         [self.hud setHidden:YES];
         [self.hud removeFromSuperview];
@@ -687,18 +552,5 @@
         [self presentViewController:mainViewController animated:YES completion:nil];
     }
 }
-
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-   // TruckViewController *ViewController = (TruckViewController *) segue.destinationViewController;
-   // ViewController.currPeripheral = self.currPeripheral;
-   // ViewController.characteristic = self.characteristic;
-}
-
-
 
 @end
